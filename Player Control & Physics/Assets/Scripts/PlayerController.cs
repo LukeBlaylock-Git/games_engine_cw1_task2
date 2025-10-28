42d9db3f6353778e3ec9bce87b3143c440850471
LancashireladOnGitHub <lukeblaylock35@gmail.com>
2025-10-28 16:23:55 +0000

    Gravity + Velocity variables
    Start of a Jump button, required velocity and gravity to be implemented.

--- Player Control & Physics/Assets/Scripts/PlayerController.cs
+++ Player Control & Physics/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     float rotation_speed = 500f;
     Vector3 velocity;
     private CharacterController character_controller;
+    private float downward_velocity; //stores the velocity of the player going... well down.
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,12 +27,23 @@ public class PlayerController : MonoBehaviour
 
         velocity = new Vector3(horizontal, 0f, vertical) * movementspeed;
 
+        if (character_controller.isGrounded)
+        {
+            downward_velocity = -2f; //locks the player to the ground, good for slopes.
+        }
+        else
+        {
+            downward_velocity += Physics.gravity.y * Time.deltaTime; //uses the rigidbody and inbuilt gravity system of unity.
+        }
+
+        velocity.y = downward_velocity;
         character_controller.Move(velocity * Time.deltaTime);
+  
 
         if (move_amount > 0)
         {
-            var target_rotation = Quaternion.LookRotation(velocity); //This is just to make sure our character turns with our movement
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, 500f * Time.deltaTime);
+            var target_rotation = Quaternion.LookRotation(new Vector3(velocity.x, 0f, velocity.z)); 
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, 500f * Time.deltaTime); //This is just to make sure our character turns with our movement
         }
     }
 }
